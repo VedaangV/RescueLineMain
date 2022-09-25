5de20058f21b9f3bb3ea64a26389e308925bc9b3
aditi-g54 <42848205+ilovebook54@users.noreply.github.com>
2022-09-25 14:42:23 -0400

    Update Intersections.cpp

--- Intersections.cpp
+++ Intersections.cpp
@@ -1,2 +1,64 @@
-//Code for intersections.
-//this is a test
+//BASIC LOGIC FOR GREENSQ
+
+
+void setup() 
+{
+  Wire.begin();
+  Serial.begin(9600);
+  //setup for EV3 color
+
+}
+
+void loop() 
+{
+  greensq();
+}
+
+void get_color()
+{
+  int temp_color; //temporary color readings
+  int green_check; //check against certain ratio (ex: green/red)
+
+  if(temp_color > green_check)
+  {
+    return(1);
+  }
+
+  else
+  {
+    return(0);
+  }
+  
+}
+
+
+void greensq()
+{
+  int rcolor = get_color();
+  int lcolor = get_color();
+
+  if (rcolor == 1 || lcolor == 1)
+  {
+    //perhaps insert code to prevent over-turning???
+    int green_val = (rcolor << 1) + lcolor;
+
+    /*switch (green_val)
+    {
+      case 3:
+        enc_turn(180);
+        break;
+
+      case 2:
+        enc_turn(90);
+        break;
+
+      case 3:
+        enc_turn(90);
+        break;
+
+      default:
+        break;
+    }*/
+    
+  }
+}
