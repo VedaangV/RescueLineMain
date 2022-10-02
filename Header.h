15c19fa5afa40fa55c4d65f9d74e618739abdc5c
VedaangV <97842612+VedaangV@users.noreply.github.com>
2022-10-02 14:07:13 -0400

    Update Header.h

--- Header.h
+++ Header.h
@@ -1,6 +1,9 @@
 #pragma once
 //libraries, global vars, and essential functions in here:
-
+#include <Adafruit_BNO055.h>
+#define BNO055_SAMPLERATE_DELAY_MS (10)
+Adafruit_BNO055 bno = Adafruit_BNO055(-1, 0x28);
+imu::Vector<3> rot;
 //
 //all func declarations.
 void get_color();
