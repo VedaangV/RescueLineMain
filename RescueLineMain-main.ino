5e4cc900eb86357ac035b4de1407ca16a17c50ef
VedaangV <97842612+VedaangV@users.noreply.github.com>
2022-10-09 01:14:07 -0400

    Update RescueLineMain-main.ino

--- RescueLineMain-main.ino
+++ RescueLineMain-main.ino
@@ -2,9 +2,14 @@
 #include "Header.h"
 void setup() {
   //setup in here
+  Serial.begin(9600);
+  Wire.begin();
+  attachInterrupt(digitalPinToInterrupt(18), Interruptfunc, RISING);
+
+
 }
 void loop() {
   //main loop in here
   
+  
 }
-
