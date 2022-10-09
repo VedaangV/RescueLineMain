d2085bef2bcba78d1090903ba8f811a4fe716a11
VedaangV <97842612+VedaangV@users.noreply.github.com>
2022-10-09 01:16:12 -0400

    Update LineTracing.cpp

--- LineTracing.cpp
+++ LineTracing.cpp
@@ -1,7 +1,13 @@
 //code for line tracing.
+#include "Header.h"
+const int sensorCount = 8;//# of qtr array sensors
+int bw_vals[sensorCount]; //array for qtr vals
+float integral = 0.0;
+float derivative = 0.0;
+float last_error = 0.0;
 QTRSensors qtr;
 void setup_qtr() {
-  int qtr_pins[] = {22, 23, 24, 25, 26, 27, 28, 29};
+  const uint8_t qtr_pins[] = {22, 23, 24, 25, 26, 27, 28, 29};
   
   qtr.setTypeRC();
   qtr.setEmitterPin(30);
@@ -9,40 +15,52 @@ void setup_qtr() {
 
   for (int i = 0; i < 100; i++) {//100 iterations
     qtr.calibrate();
-    qtr.setSensorPins((const uint8_t[]) qtr_pins[], sensorCount);
+    qtr.setSensorPins(qtr_pins, sensorCount);
   }
 }
 float error_calc(){
   
-  float target_vals[] = {0, 0, 0, 0};//desired difference between sensor pairs
+  float target_vals[] = {48, -808, 48, 108};//desired difference between sensor pairs
   float multipliers[] = {1, 1, 1, 1};//multipliers for each sensor pair
   float error = 0.0;//error in PID
   qtr.read(bw_vals);
   for(int i = 0; i < sensorCount / 2; i++){//iterates through all sensor pairs, outer to inner
-    float current_val = (bw_vals[sensorCount - i] - bw_vals[i])//difference between current pair of sensors
-    float error += (target_vals[i] - current_val) * multipliers[i]; //difference between desired difference of sensor vals and actual difference of sensor vals. Each pair has its own multiplier.
+    float current_val = (bw_vals[7 - i] - bw_vals[i]);//difference between current pair of sensors
+    error += (target_vals[i] - current_val) * multipliers[i]; //difference between desired difference of sensor vals and actual difference of sensor vals. Each pair has its own multiplier.
   }
   return error;
 }
 void qtr_print(){
   qtr.read(bw_vals);
   for(int i = 0; i < sensorCount; i++){
-    Serial.println("Sensor %d: %d", i, bw_vals[i]);
+    Serial.print("Sensor ");
+    Serial.print(i);
+    Serial.print(": ");
+    Serial.println(bw_vals[i]);
+    
   }
 }
 void pid_print(){
-  Serial.println("PID error: %f", error_calc());
+  Serial.print("PID error: "); Serial.println(error_calc());
+  
+}
+void diff_print(){
+  for(int i = 0; i < sensorCount/2; i++){
+    Serial.print("Pair ");
+    Serial.print(i);
+    Serial.print(": ");
+    Serial.println(bw_vals[7 - i] - bw_vals[i]);
+  }
 }
 void lineTrace(){
-  const int base_speed = 50;
-  const float kp = 1.0;
+  const int base_speed = 65;
+  const float kp = 0.01;
   const float ki = 0.0;
   const float kd= 0.0;
   float error = error_calc();
   integral += error;
   derivative = error - last_error;
   float adjustSpeed = (error * kp) + (integral * ki) + (derivative * kd);
-  lmotor.run(base_speed + adjustSpeed);
-  rmotor.run(base_speed - adjustSpeed);
+  setMultipleMotors(base_speed + adjustSpeed, base_speed - adjustSpeed);
   last_error = error;
 }
