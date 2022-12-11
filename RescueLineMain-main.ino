574135800b29872bedf038da15ae0250556c50c5
ae9000 <115202201+ae9000@users.noreply.github.com>
2022-12-11 13:37:10 -0500

    Update RescueLineMain-main.ino

--- RescueLineMain-main.ino
+++ RescueLineMain-main.ino
@@ -4,8 +4,9 @@ void setup() {
   //setup in here
   Serial.begin(9600);
   Wire.begin();
+  //ultrasonic isr
   attachInterrupt(digitalPinToInterrupt(18), Interruptfunc, RISING);
-
+   attachInterrupt(digitalPinToInterrupt(2), distanceISR,CHANGE);
 
 }
 void loop() {
