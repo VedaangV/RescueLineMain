2b4d599a488ab108334f2ee09d21be6dfbdaa27a
ae9000 <115202201+ae9000@users.noreply.github.com>
2022-12-11 13:16:29 -0500

    Add files via upload

---
+++ Get_Distance.c
@@ -0,0 +1,34 @@
+//required variables
+//ultrasonic
+volatile long pulseMsSt = 0;
+volatile long pulseMsEd = 0;
+volatile bool pulseFirst = 1;
+
+//isr that is needed
+ attachInterrupt(digitalPinToInterrupt(2), distanceISR,CHANGE);
+
+void distanceISR()
+{
+ if(pulseFirst)
+ {
+  pulseMsSt= micros(); 
+  pulseFirst = 0;
+ }
+ else
+ {
+  pulseMsEd = micros();
+  pulseFirst = 1;
+ }
+}
+
+float getFrontDistance()
+{
+  long pulseTime = pulseMsEd - pulseMsSt;
+  float distance;
+  distance = pulseTime * 0.034 / 2;
+  if(distance < 0)
+  {
+    distance = 0;
+  }
+  return(distance);
+}
