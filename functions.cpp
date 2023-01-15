0877b0023cfea980193d0a045d3942d1c097cd98
VedaangV <97842612+VedaangV@users.noreply.github.com>
2023-01-15 12:14:21 -0500

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -1,4 +1,4 @@
-//below are essential functions, used all across our project. 
+//below are essential functions, used all across our project.
 #include "Header.h"
 #include "MeMegaPi.h"
 MeMegaPiDCMotor lmotor(PORT1B);//left motor
@@ -19,36 +19,78 @@ void Interruptfunc() {
   }
 }
 
-void forward_enc(int encoders, int motorSpeed) {//go forward for enc
+void forward_enc(int encoders, int motor_speed) {//go forward for enc
   enc = 0;
   while (enc <= encoders) {
-    lmotor.run(50);
-    rmotor.run(-50);
+    lmotor.run(motor_speed);
+    rmotor.run(-1 * motor_speed);
   }
   setMultipleMotors(0, 0);
 }
-
-void forwardCm(float dist) {//go forward for cm
+void backward_enc(int encoders, int motor_speed) {//go backward for enc
   enc = 0;
-  while (enc <= cm_to_encoders(dist)) {
-    setMultipleMotors(50, 50);
+  while (enc >= -1 * encoders) {
+    lmotor.run(-1 * motor_speed);
+    rmotor.run(motor_speed);
   }
   setMultipleMotors(0, 0);
 }
+void backwardCm(float dist, int motor_speed) { //go backward for cm
+  backward_enc(cm_to_encoders(dist), motor_speed);
+}
+void forwardCm(float dist, int motor_speed) {//go forward for cm
+  forward_enc(cm_to_encoders(dist), motor_speed);
+}
 
-void setMultipleMotors(int left, int right){
+void setMultipleMotors(int left, int right) { //sets motors
   lmotor.run(left);
-  rmotor.run(right);
+  rmotor.run(-1 * right);
 }
-void go_motors(int motorSpeed){
-  lmotor.run(motorSpeed);
-  rmotor.run(motorSpeed);
-}
-void rturn(int motorSpeed){
+void go_motors(int motorSpeed) { //goes forward
   lmotor.run(motorSpeed);
   rmotor.run(-1 * motorSpeed);
 }
-void lturn(int motorSpeed){
-  lmotor.run(-1 * motorSpeed);
+void basic_turn(int motorSpeed) { //basic turn
+  lmotor.run(motorSpeed);
   rmotor.run(motorSpeed);
+}
+void rturn(int motorSpeed) { //right turn
+  lmotor.run(motorSpeed);
+  rmotor.run(motorSpeed);
+}
+void lturn(int motorSpeed) { //left turn
+  lmotor.run(-1 * motorSpeed);
+  rmotor.run(-1 * motorSpeed);
+}
+void tcaselect(uint8_t i) {
+  if (i > 7) return;
+  Wire.beginTransmission(TCAADDR);
+  Wire.write(1 << i);
+  Wire.endTransmission();
+}
+void rightMotorRun(int motorSpeed) { //move right motor
+  rmotor.run(-1 * motorSpeed);
+}
+void leftMotorRun(int motorSpeed) { //move left motor
+  lmotor.run(motorSpeed);
+}
+void motorsStop() { //stop all motors
+  lmotor.run(0);
+  rmotor.run(0);
+}
+int getCase() { //checks for all important cases during a run. Based on the return val, the main function is determined.
+  /*if(<silver condition>){EVAC
+    return 1;
+    }*/
+
+  if (get_color(6.5) != 0) {
+    return 2;
+  }
+
+  if (seeObs(7.0)) { //OBSTACLE
+    return 3;
+  }
+
+  return 0;
+
 }
