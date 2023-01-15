aedbdb74506a306e6b2111cac9658d348c69a475
VedaangV <97842612+VedaangV@users.noreply.github.com>
2023-01-15 12:12:36 -0500

    Update Intersections.cpp

--- Intersections.cpp
+++ Intersections.cpp
@@ -1,51 +1,132 @@
 //BASIC LOGIC FOR GREENSQ
 #include "Header.h"
+float meanLeft = 0.0;
+float meanRight = 0.0;
+float greenWhiteThresh = 501.0;
+float greenBlackThresh = 1499.0;
+int s1[6];
+int s2[6];
 
-int get_color()
+void get_vals() { //gets readings from both AS7262 color sensors
+
+  Serial2.println("ATDATA");
+  Serial3.println("ATDATA");
+  get_ok();
+  s1[0] = Serial2.parseInt();
+  s1[1] = Serial2.parseInt();
+  s1[2] = Serial2.parseInt();
+  s1[3] = Serial2.parseInt();
+  s1[4] = Serial2.parseInt();
+  s1[5] = Serial2.parseInt();
+  s2[0] = Serial3.parseInt();
+  s2[1] = Serial3.parseInt();
+  s2[2] = Serial3.parseInt();
+  s2[3] = Serial3.parseInt();
+  s2[4] = Serial3.parseInt();
+  s2[5] = Serial3.parseInt();
+
+  //print vals and calculate the mean on left and right
+
+  Serial.println("Left values (vbgyor): ");
+
+
+  for (int i = 0; i < 6; i++) {
+    Serial.print(s2[i]);
+    Serial.print(" ");
+    meanLeft += s2[i];
+  }
+
+  meanLeft /= 6;
+  Serial.print("\nMean Left: ");
+  Serial.print(meanLeft);
+  Serial.println("\n");
+  Serial.println("Right values (vbgyor): ");
+
+  for (int i = 0; i < 6; i++) {
+    Serial.print(s1[i]);
+    Serial.print(" ");
+    meanRight += s1[i];
+  }
+
+  meanRight /= 6;
+  Serial.print("\nMean Right: ");
+  Serial.print(meanRight);
+}
+
+void get_ok()//communication through serial for color sensors
 {
-  int temp_color; //temporary color readings
-  int green_check; //check against certain ratio (ex: green/red)
+  Serial.println("in get ok");
 
-  if(temp_color > green_check)
+  while (Serial2.available())
   {
-    return 1;
-  }
 
-  else
-  {
-    return 0;
+
+    if (Serial2.read() == 'K')
+    {
+      Serial.println("found 2 ok");
+      break;
+    }
   }
-  
+  while (Serial3.available())
+  {
+
+    if (Serial3.read() == 'K')
+    {
+      Serial.println("found 3 ok");
+      break;
+    }
+  }
 }
 
 
-void greensq()
+
+
+int get_color(float green_check)//checks for green based on color vals
 {
-  int rcolor = get_color();
-  int lcolor = get_color();
+  bool rcolor = 0;
+  bool lcolor = 0;
 
-  if (rcolor == 1 || lcolor == 1)
+  if (s1[1] / s1[5] >= green_check)
   {
-    //perhaps insert code to prevent over-turning???
-    int green_val = (rcolor << 1) + lcolor;
-
-    /*switch (green_val)
-    {
-      case 3:
-        enc_turn(180);
-        break;
-
-      case 2:
-        enc_turn(90);
-        break;
-
-      case 3:
-        enc_turn(90);
-        break;
-
-      default:
-        break;
-    }*/
-    
+    Serial.println("right green");
+    rcolor = 1;
   }
+
+  if (s2[1] / s2[5] >= green_check)
+  {
+    Serial.println("left green");
+    lcolor = 1;
+  }
+
+  return ((rcolor << 1) + lcolor);
+
+}
+
+void greensq()//checks for green and moves accordingly
+{
+  //perhaps insert code to prevent over-turning???
+  motorsStop();
+  switch (get_color(6.5))
+  {
+    case 3:
+      Serial.println("3");
+      enc_turn(180, 100);
+      break;
+    case 2:
+      Serial.println("2");
+      forwardCm(1.5, 70);
+      enc_turn(90, 100);
+      break;
+    case 1:
+      Serial.println("1");
+      forwardCm(1.5, 70);
+      enc_turn(-90, 100);
+      break;
+    default:
+      Serial.println("default/0");
+      break;
+  }
+
+
+
 }
