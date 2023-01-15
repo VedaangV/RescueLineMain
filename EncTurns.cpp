657d16ef8b62f10514fc8d4380835955f2d3f52e
VedaangV <97842612+VedaangV@users.noreply.github.com>
2023-01-15 12:11:37 -0500

    Update EncTurns.cpp

--- EncTurns.cpp
+++ EncTurns.cpp
@@ -1,28 +1,82 @@
-void enc_turn(int deg, int speed)
+#include "Header.h"
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
+int BNO055_SAMPLERATE_DELAY_MS = (10);
+imu::Vector<3> rot;
+int BNO = 2;
+float yaw = 0.0;
+
+const int m1_forward = -75;
+const int m2_forward = 75;
+//****************************************************************************************************************\\
+
+
+void enc_turn(int deg, int speed)//function for turning a specific amount of degrees.
 {
-  int target = 0;
+  tcaselect(BNO);
+  int target = deg;
   motorsStop();
   yaw = getYaw();
-  target = yaw + deg;
 
-  if (target < 0)
+  //right turn
+  if (deg > 0)
   {
-    target+= 360;
-  }
-
-  else if (target > 360)
-  {
-    target -= 360;
-  }
-
-  int calc_speed = ((deg * -1) * speed) / abs(deg);
-
-  while (yaw > target + 2 || yaw < target - 2)
+    //target = yaw + deg;
+    if (target > 360)
     {
-      rightMotorRun(calc_speed);
-      leftMotorRun(-calc_speed);
+      target = target - 360;
+    }
+
+    while (/*yaw > target + 1 ||*/ yaw < target - 1)
+    {
+      rightMotorRun(-speed);
+      leftMotorRun(speed);
+      yaw = getYaw();
+      Serial.print("Yaw: ");
+      Serial.println(yaw);
+    }
+    motorsStop();
+  }
+
+  //left turn
+  if (deg < 0)
+  {
+    if (yaw < abs(deg))
+    {
+      target = yaw + (360 - abs(deg));
+    }
+
+    else
+    {
+      target = yaw - abs(deg);
+    }
+    while (yaw > target + 2 /*|| yaw < target - 2*/)
+    {
+      rightMotorRun(speed);
+      leftMotorRun(-speed);
       yaw = getYaw();
     }
-    
-  motorsStop();
+
+    while (yaw > target)
+    {
+      lturn(speed);
+      yaw = getYaw();
+    }
+    motorsStop();
+  }
+
+}
+float getYaw() { //gets yaw from BNO
+  tcaselect(BNO);
+  sensors_event_t orientationData;
+  bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
+  return orientationData.orientation.x;
+}
+void bnoSetup() { //bno init
+  tcaselect(BNO);
+  if (!bno.begin(8))
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while (1) {};
+  }
 }
