4220975966885e2813cde671c745bd16244ea0b5
aditi-g54 <42848205+ilovebook54@users.noreply.github.com>
2023-02-12 13:03:59 -0500

    Update Intersections.cpp

--- Intersections.cpp
+++ Intersections.cpp
@@ -1,158 +1,190 @@
-//BASIC LOGIC FOR GREENSQ
-#include "Header.h"
-float meanLeft = 0.0;
-float meanRight = 0.0;
-//float greenWhiteThresh = 501.0;
-//float greenBlackThresh = 1499.0;
-int s1[6];
-int s2[6];
+//v8
+float s1[6];
+float s2[6];
+//float gr_avg = 0;
 
-void get_vals() { //gets readings from both AS7262 color sensors
+float green_check = 13;
+int green_count = 0;
 
+void get_ok2()
+{
+  //Serial.println("Serail 2 Available  ");
+  //Serial.println(Serial2.available());
+
+  while(Serial2.available())
+  {
+    if(Serial2.read() == 'K')
+    {
+      //Serial.println("found ok");
+      break;
+    }
+  }
+  
+}
+
+void get_ok3()
+{
+  //Serial.println("Serail 3 Available  ");
+  //Serial.println(Serial3.available());
+
+  while(Serial3.available())
+  {
+    if(Serial3.read() == 'K')
+    {
+      //Serial.println("found ok");
+      break;
+    }
+  }
+  
+}
+
+//****************************************************************************************************************\\
+
+
+void get_vals()
+{
   Serial2.println("ATDATA");
-  get_ok();
+  get_ok2();
+
   s1[0] = Serial2.parseInt();
   s1[1] = Serial2.parseInt();
   s1[2] = Serial2.parseInt();
   s1[3] = Serial2.parseInt();
   s1[4] = Serial2.parseInt();
   s1[5] = Serial2.parseInt();
+  
+  /*Serial.print(s1[0]);
+  Serial.print("  ");
+  Serial.print(s1[1]);
+  Serial.print("  ");
+  Serial.print(s1[2]);
+  Serial.print("  ");
+  Serial.print(s1[3]);
+  Serial.print("  ");
+  Serial.print(s1[4]);
+  Serial.print("  ");
+  Serial.print(s1[5]);
+  Serial.println("  ");*/
+
+  
+
+
+//---------------------------------------------------------------------------\\
+
 
   Serial3.println("ATDATA");
-  get_ok();
+  get_ok3();
+
+  
   s2[0] = Serial3.parseInt();
   s2[1] = Serial3.parseInt();
   s2[2] = Serial3.parseInt();
   s2[3] = Serial3.parseInt();
   s2[4] = Serial3.parseInt();
   s2[5] = Serial3.parseInt();
+  
 
-  /* Serial.print("s1: ");
-    Serial.print(s1[2]/s1[5]);
-    Serial.print(" S1 GREEN: ");
-    Serial.println(s1[2]);
-    Serial.print("s2: ");
-    Serial.print(s2[2]/s2[5]);
-    Serial.print(" S2 GREEN: ");
-    Serial.println(s2[2]);*/
+  /*Serial.print(s2[0]);
+  Serial.print("  ");
+  Serial.print(s2[1]);
+  Serial.print("  ");
+  Serial.print(s2[2]);
+  Serial.print("  ");
+  Serial.print(s2[3]);
+  Serial.print("  ");
+  Serial.print(s2[4]);
+  Serial.print("  ");
+  Serial.print(s2[5]);
+  Serial.println("  ");*/
 
-  //print vals and calculate the mean on left and right
-
-  Serial.println("Left values (vbgyor): ");
-
-
-  for (int i = 0; i < 6; i++) {
-    Serial.print(s2[i]);
-    Serial.print(" ");
-    meanLeft += s2[i];
-  }
-
-  meanLeft /= 6;
-  Serial.print("\nMean Left: ");
-  Serial.print(meanLeft);
-  Serial.println("\n");
-  Serial.println("Right values (vbgyor): ");
-
-  for (int i = 0; i < 6; i++) {
-    Serial.print(s1[i]);
-    Serial.print(" ");
-    meanRight += s1[i];
-  }
-
-  meanRight /= 6;
-  Serial.print("\nMean Right: ");
-  Serial.print(meanRight);
-}
-
-void get_ok()//communication through serial for color sensors
-{
-  //Serial.println("in get ok");
-
-  while (Serial2.available())
+  //Since we are using the g/r ratio to detect green, r cannot be equal to zero. if r == 0, set it to 1
+  if(s1[5] == 0)
   {
-
-
-    if (Serial2.read() == 'K')
-    {
-      //Serial.println("found 2 ok");
-      break;
-    }
+    s1[5] = 1;
   }
-  while (Serial3.available())
+
+  if(s2[5] == 0)
   {
-
-    if (Serial3.read() == 'K')
-    {
-      //Serial.println("found 3 ok");
-      break;
-    }
+    s2[5] = 1;
   }
+
+  
 }
 
 
 
 
-int get_color(float green_check)//checks for green based on color vals
+
+
+int get_color()//checks for green based on color vals
 {
   get_vals();
-
+ 
   bool rcolor = 0;
   bool lcolor = 0;
 
-  if ((s1[2] / s1[5] >= green_check && meanRight < 12))
+   
+  if (s1[2]/s1[5] >= green_check)
   {
     //Serial.println("right green");
     rcolor = 1;
   }
 
-  if (s2[2] / s2[5] >= green_check && meanLeft < 12)
+  if (s2[2]/s2[5] >= green_check)
   {
     //Serial.println("left green");
     lcolor = 1;
   }
 
-
-  return ((rcolor * 2) + lcolor);
+  return ((rcolor << 1) + lcolor);
 
 }
-
 
 void greensq()//checks for green and moves accordingly
 {
   //perhaps insert code to prevent over-turning???
-  switch (get_color(12))
+  //motorsStop();
+  switch (get_color())
   {
     case 3:
-      motorsStop();
-      delay(700);
-      Serial.println("3");
-      forwardCm(5.5, 70);
-      enc_turn(180, 100);
-      backwardCm(1.0, 70);
+      Serial.println("check 3");
+      green_count++;
+      if(green_count > 6)
+      {
+        //enc_turn(180, 100);
+        Serial.print("GREEN 3");
+        green_count = 0;
+      }
       break;
+      
     case 2:
-      motorsStop();
-      delay(700);
-      Serial.println("2");
-      forwardCm(5.5, 70);
-      enc_turn(90, 100);
-      backwardCm(1.0, 70);
-
+      Serial.println("check 2");
+      green_count++;
+      if(green_count > 6)
+      {
+        //forwardCm(1.5, 70);
+        //enc_turn(90, 100);
+        Serial.print("GREEN 2");
+        green_count = 0;
+      }
       break;
+
     case 1:
-      motorsStop();
-      delay(700);
-      Serial.println("1");
-      forwardCm(5.5, 70);
-      enc_turn(-90, 100);
-      backwardCm(1.0, 70);
-
+      Serial.println("check 1");
+      green_count++;
+      if(green_count > 6)
+      {
+        //forwardCm(1.5, 70);
+        //enc_turn(-90, 100);
+        Serial.print("GREEN 1");
+        green_count = 0;
+      }
       break;
+      
     default:
-      Serial.println("default/0");
+      Serial.println("0");
+      green_count = 0;
       break;
   }
-
-
 
 }
