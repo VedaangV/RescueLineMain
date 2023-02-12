b91012aad348bf48755973c26483a28fb1812073
ae9000 <115202201+ae9000@users.noreply.github.com>
2023-02-12 14:40:28 -0500

    Update RescueLineMain-main.ino

--- RescueLineMain-main.ino
+++ RescueLineMain-main.ino
@@ -5,38 +5,40 @@ void setup() {
   setup_qtr();
   Wire.begin();
   Serial.begin(115200);
-  Serial2.begin(115200);
-  Serial3.begin(115200);
   bnoSetup();
   Wire.begin();
   attachInterrupt(digitalPinToInterrupt(18), Interruptfunc, RISING);
   delay(100);
+  
+  Serial2.begin(115200);
+  Serial2.println("ATINTTIME=1");
+  Serial2.println("ATTCSM=1");  
+  while(Serial2.available()==0)
+  {
+    Serial.println("Serial 2 not available");
+  }
+  Serial.println("serial 2 available");
+  get_ok2();
 
+  Serial3.begin(115200);
+  Serial3.println("ATINTTIME=1");
+  Serial3.println("ATTCSM=1");  
+
+  while(Serial3.available()== 0)
+  {
+    Serial.println("Serial 3 not available");
+  }
+  Serial.println("serial 3 available");
+  get_ok3();
+  delay(2000);
+
+  //led pins
+  pinMode(A9, OUTPUT); //blue
+  pinMode(A10, OUTPUT); //green
+  pinMode(A11, OUTPUT); //red
 }
 void loop() {
-  //main loop in here
-  switch (getCase()) { //getCase() defined in FUNCTIONS.CPP
-
-    case 1:
-      //evac
-      break;
-
-    case 2:
-      motorsStop();
-      greensq();
-      break;
-
-    case 3:
-      obstacle();
-      break;
-
-    default:
-      lineTrace();
-      break;
-
-
-  }
-
-
-
+  lineTrace();
+  greensq();
+  //enc_turn(90,150);
 }
