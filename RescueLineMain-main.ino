bed119b4bc76066a9d0467d7d3466f81e7897df5
VedaangV <97842612+VedaangV@users.noreply.github.com>
2023-03-12 12:24:08 -0400

    Update RescueLineMain-main.ino

--- RescueLineMain-main.ino
+++ RescueLineMain-main.ino
@@ -1,44 +1,22 @@
-//main rescue line code
+  //main rescue line code
 #include "Header.h"
 void setup() {
   //setup in here
   setup_qtr();
   Wire.begin();
-  Serial.begin(115200);
+  Serial.begin(115200); Serial2.begin(115200); Serial3.begin(115200);
+  Serial2.println("ATINTTIME=1"); get_ok2();
+  Serial3.println("ATINTTIME=1"); get_ok3();
   bnoSetup();
   Wire.begin();
   attachInterrupt(digitalPinToInterrupt(18), Interruptfunc, RISING);
-  delay(100);
-  
-  Serial2.begin(115200);
-  Serial2.println("ATINTTIME=1");
-  Serial2.println("ATTCSM=1");  
-  while(Serial2.available()==0)
-  {
-    Serial.println("Serial 2 not available");
-  }
-  Serial.println("serial 2 available");
-  get_ok2();
-
-  Serial3.begin(115200);
-  Serial3.println("ATINTTIME=1");
-  Serial3.println("ATTCSM=1");  
-
-  while(Serial3.available()== 0)
-  {
-    Serial.println("Serial 3 not available");
-  }
-  Serial.println("serial 3 available");
-  get_ok3();
-  delay(2000);
-
   //led pins
-  pinMode(A9, OUTPUT); //blue
-  pinMode(A10, OUTPUT); //green
-  pinMode(A11, OUTPUT); //red
+  pinMode(A9, OUTPUT);
+  pinMode(A10, OUTPUT);
+  pinMode(A11, OUTPUT);
 }
 void loop() {
-  lineTrace();
-  greensq();
-  //enc_turn(90,150);
+ greensq();
+ obstacle();
+ linetrace();
 }
