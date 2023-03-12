f1249a648c4911553a48b9b54860a465211e96f7
VedaangV <97842612+VedaangV@users.noreply.github.com>
2023-03-12 12:21:17 -0400

    Update Obstacle.cpp

--- Obstacle.cpp
+++ Obstacle.cpp
@@ -6,38 +6,9 @@ volatile long pulseMsEd = 0;
 volatile bool pulseFirst = 1;
 const int pingPin = 39;
 
-long microToInch(long microseconds) {
-  return microseconds / 29 / 2;
-}
-
-long microToCm(long microseconds) {
-  return microseconds / 73.746 / 2;
-}
-
-long getCm() { //get Cm from ping sensor
-  pinMode(pingPin, OUTPUT);//set as output
-  digitalWrite(pingPin, LOW);//start with low pulse for clean high
-  delayMicroseconds(2);
-  digitalWrite(pingPin, HIGH);
-  delayMicroseconds(5);
-  digitalWrite(pingPin, LOW);
-  pinMode(pingPin, INPUT);//set as input
-  duration = pulseIn(pingPin, HIGH);//read input in microseconds
-  return microToCm(duration);//convert micro s to cm
-}
-long getInch() { //get inches from ping sensor
-  pinMode(pingPin, OUTPUT);//set as output
-  digitalWrite(pingPin, LOW);//start with low pulse for clean high
-  delayMicroseconds(2);
-  digitalWrite(pingPin, HIGH);
-  delayMicroseconds(5);
-  digitalWrite(pingPin, LOW);
-  pinMode(pingPin, INPUT);//set as input
-  duration = pulseIn(pingPin, HIGH);//read input in microseconds
-  return microToInch(duration);//convert micro s to inch
-}
 void distanceISR()
 {
+  
   if (pulseFirst)
   {
     pulseMsSt = micros();
@@ -52,7 +23,12 @@ void distanceISR()
 
 float getFrontDistance()//get distance of Front US
 {
-  long pulseTime = pulseMsEd - pulseMsSt;
+    digitalWrite(5, LOW);
+  delay(1);
+  digitalWrite(5, HIGH);
+  delay(1);
+  digitalWrite(5, LOW);
+  long pulseTime = pulseIn(2, HIGH);
   float distance;
   distance = pulseTime * 0.034 / 2;
   if (distance < 0)
@@ -72,15 +48,14 @@ bool seeObs(long dist) { //sees obstacle?
 }
 
 void avoid(int sign) { //move around obstacle
-  forwardCm(7.0, 70);
+  forwardCm(15.0, 70);
   enc_turn(-90 * sign, 70);
-  forwardCm(17.0, 70);
+  forwardCm(40.0, 70);
   enc_turn(-90 * sign, 70);
-  forwardCm(7.0, 70);
+  forwardCm(15.0, 70);
   enc_turn(90 * sign, 70);
 }
 void obstacle() { //main obstacle function
-  if (seeObs(5.0)) {
     enc_turn(90, 70);
     if (seeObs(10.0)) { //sees wall
       enc_turn(180, 70);
@@ -89,6 +64,4 @@ void obstacle() { //main obstacle function
     else {
       avoid(1);
     }
-  }
 }
-
