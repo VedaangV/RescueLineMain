3d54b475e9cc3bce1e487048dcad8489e65a8aa2
VedaangV <97842612+VedaangV@users.noreply.github.com>
2023-03-19 14:51:32 -0400

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -3,11 +3,11 @@
 volatile int enc = 0;
 
 float cm_to_encoders(float cm) {
-  const float wheelDiameter = 6.0;
+  const float wheelDiameter = 6.42;
   return cm / (wheelDiameter * PI) * 360;
 }
 
-void Interruptfunc() {
+void Interruptfunc() {//interrupt func for encoders
   if (digitalRead(31) == HIGH) {
     enc--;
   }
@@ -19,14 +19,14 @@ void Interruptfunc() {
 void forward_enc(int encoders, int motor_speed) {//go forward for enc
   enc = 0;
   while (enc <= encoders) {
-    setMultipleMotors(motor_speed,motor_speed);
+    setMultipleMotors(motor_speed, motor_speed);
   }
   setMultipleMotors(0, 0);
 }
 void backward_enc(int encoders, int motor_speed) {//go backward for enc
   enc = 0;
   while (enc >= -1 * encoders) {
-    setMultipleMotors(-motor_speed,-motor_speed);
+    setMultipleMotors(-motor_speed, -motor_speed);
   }
   setMultipleMotors(0, 0);
 }
@@ -39,94 +39,78 @@ void forwardCm(float dist, int motor_speed) {//go forward for cm
 
 
 
-void setMultipleMotors(int left, int right){
+void setMultipleMotors(int left, int right) { //set motors
   //port 1
   left = left > 255 ? 255 : left;
   left = left < -255 ? -255 : left;
- 
-  if(left > 0)
+
+  if (left > 0)
   {
     digitalWrite(35, LOW);
     delayMicroseconds(5);
     digitalWrite(34, HIGH);
-    analogWrite(12,left);//pwm pin 12
+    analogWrite(12, left); //pwm pin 12
   }
-  else if(left < 0)
+  else if (left < 0)
   {
     digitalWrite(34, LOW);
     delayMicroseconds(5);
     digitalWrite(35, HIGH);
-    analogWrite(12,-left);
+    analogWrite(12, -left);
   }
   else
   {
     digitalWrite(34, LOW);
     digitalWrite(35, LOW);
-    analogWrite(12,0);
+    analogWrite(12, 0);
   }
 
   right = left > 255 ? 255 : right;
   right = right < -255 ? -255 : right;
   right = -right;
-  if(right > 0)
+  if (right > 0)
   {
     digitalWrite(36, LOW);
     delayMicroseconds(5);
     digitalWrite(37, HIGH);
-    analogWrite(8,right);
+    analogWrite(8, right);
   }
-  else if(right < 0)
+  else if (right < 0)
   {
     digitalWrite(37, LOW);
     delayMicroseconds(5);
     digitalWrite(36, HIGH);
-    analogWrite(8,-right);
+    analogWrite(8, -right);
   }
   else
   {
     digitalWrite(36, LOW);
     digitalWrite(37, LOW);
-    analogWrite(8,0);
+    analogWrite(8, 0);
   }
 
 }
 void go_motors(int motorSpeed) { //goes forward
-  setMultipleMotors(motorSpeed,motorSpeed);
+  setMultipleMotors(motorSpeed, motorSpeed);
 }
 void rturn(int motorSpeed) { //right turn
-  setMultipleMotors(motorSpeed,-motorSpeed);
+  setMultipleMotors(motorSpeed, -motorSpeed);
 }
 void lturn(int motorSpeed) { //left turn
-  setMultipleMotors(-motorSpeed,motorSpeed);
+  setMultipleMotors(-motorSpeed, motorSpeed);
 }
-void tcaselect(uint8_t i) {
+void tcaselect(uint8_t i) {//selects tca channel
   if (i > 7) return;
   Wire.beginTransmission(TCAADDR);
   Wire.write(1 << i);
   Wire.endTransmission();
 }
 void rightMotorRun(int motorSpeed) { //move right motor
-  setMultipleMotors(0,motorSpeed);
+  setMultipleMotors(0, motorSpeed);
 }
 void leftMotorRun(int motorSpeed) { //move left motor
-  setMultipleMotors(motorSpeed,0);
+  setMultipleMotors(motorSpeed, 0);
 }
 void motorsStop() { //stop all motors
-  setMultipleMotors(0,0);
-}
-int getCase() { //checks for all important cases during a run. Based on the return val, the main function is determined.
-  /*if(<silver condition>){EVAC
-    return 1;
-    }*/
-
-  if (get_color(6.5) != 0) {
-    return 2;
-  }
-
-  if (seeObs(7.0)) { //OBSTACLE
-    return 3;
-  }
-
-  return 0;
-
+  setMultipleMotors(0, 0);
 }
