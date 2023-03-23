826e6a719e94c260912d3b0ee9655b3246eda34f
aditi-g54 <42848205+ilovebook54@users.noreply.github.com>
2023-03-23 19:00:38 -0400

    Update LineTracing.cpp

--- LineTracing.cpp
+++ LineTracing.cpp
@@ -1,27 +1,21 @@
 //code for line tracing.
 #include "Header.h"
 const int sensorCount = 8;//# of qtr array sensors
-int bw_vals[sensorCount]; //array for qtr vals
 float integral = 0.0;
 float derivative = 0.0;
 float last_error = 0.0;
+int bw_vals[sensorCount]; //array for qtr vals
+int counter = 0;
 QTRSensors qtr;
 void setup_qtr() {
-  const uint8_t qtr_pins[] = {22, 23, 24, 25, 26, 27, 28, 29};
-  
+  qtr.setSensorPins((const uint8_t[]){22, 23, 24, 25, 26, 30, 28, 29}, 8);
   qtr.setTypeRC();
-  qtr.setEmitterPin(30);
-  qtr.resetCalibration();
-
-  for (int i = 0; i < 100; i++) {//100 iterations
-    qtr.calibrate();
-    qtr.setSensorPins(qtr_pins, sensorCount);
-  }
+  //qtr.setEmitterPin(2);
 }
 float error_calc(){
   
-  float target_vals[] = {48, -808, 48, 108};//desired difference between sensor pairs
-  float multipliers[] = {1, 1, 1, 1};//multipliers for each sensor pair
+  float target_vals[] = {116, 0, -128, 0};//desired difference between sensor pairs
+  float multipliers[] = {1.9, 1.6, 1.2, 1.0};//multipliers for each sensor pair
   float error = 0.0;//error in PID
   qtr.read(bw_vals);
   for(int i = 0; i < sensorCount / 2; i++){//iterates through all sensor pairs, outer to inner
@@ -45,6 +39,7 @@ void pid_print(){
   
 }
 void diff_print(){
+  qtr.read(bw_vals);
   for(int i = 0; i < sensorCount/2; i++){
     Serial.print("Pair ");
     Serial.print(i);
@@ -52,15 +47,118 @@ void diff_print(){
     Serial.println(bw_vals[7 - i] - bw_vals[i]);
   }
 }
+int check_all() {//check all sensors to see if they have the same relative values
+
+  qtr.read(bw_vals);
+  int result = 0;// 0 - no uniform val, 1 - white, 2 - black
+  bool case_flag = false;
+
+  for (int i = 0; i < 8; i++) {
+    if (bw_vals[i] < 500 && result == 2) { //if see white when previously see black
+      result = 0;//different vals
+      case_flag = true;
+    }
+
+    else if (bw_vals[i] > 2300 && result == 1) { //if see black when previously white
+      result = 0;//different vals
+      case_flag = true;
+    }
+
+    else if (bw_vals[i] < 700 && !case_flag) {
+      result = 1;//all white
+    }
+
+    else if (bw_vals[i] > 2300 && !case_flag) {
+      result = 2;//all black
+    }
+
+  }
+  return result;
+}
+int check_left(){
+  int result = 0;//0-no uniform val, 1-white, 2 - black
+  bool case_flag = false;
+  for(int i = 0; i < 4; i++){
+    
+  if (bw_vals[i] < 500 && result == 2) { //if see white when previously see black
+      result = 0;//different vals
+      case_flag = true;
+    }
+
+    else if (bw_vals[i] > 2300 && result == 1) { //if see black when previously white
+      result = 0;//different vals
+      case_flag = true;
+    }
+
+    else if (bw_vals[i] < 700 && !case_flag) {
+      result = 1;//all white
+    }
+
+    else if (bw_vals[i] > 2300 && !case_flag) {
+      result = 2;//all black
+    }
+
+  }
+  return result;
+}
+int check_right(){
+  int result = 0;//0-no uniform val, 1-white, 2 - black
+  bool case_flag = false;
+  for(int i = 4; i < 7; i++){
+    
+  if (bw_vals[i] < 500 && result == 2) { //if see white when previously see black
+      result = 0;//different vals
+      case_flag = true;
+    }
+
+    else if (bw_vals[i] > 2300 && result == 1) { //if see black when previously white
+      result = 0;//different vals
+      case_flag = true;
+    }
+
+    else if (bw_vals[i] < 700 && !case_flag) {
+      result = 1;//all white
+    }
+
+    else if (bw_vals[i] > 2300 && !case_flag) {
+      result = 2;//all black
+    }
+
+  }
+  return result;
+}
 void lineTrace(){
-  const int base_speed = 65;
-  const float kp = 0.01;
-  const float ki = 0.0;
+  
+ /* if (check_all() == 2) 
+  { //if see all white (gap) or see all black (cross intersection)
+    go_motors(80);//forward speed 60
+    delay(30);
+    return;
+  }*/
+  /*if((check_left() == 2 || check_right() == 2) && counter >= 5){
+    counter = 0;
+    go_motors(150);
+    delay(90);
+    if(check_all() == 1)
+    {//see all white
+      go_motors(-150);
+      delay(90);
+    }
+  }*/
+  digitalWrite(A9, HIGH);
+  digitalWrite(A10, LOW);
+  digitalWrite(A11, LOW);
+  const int base_speed = 55;
+  const float kp = 0.05;
+  const float ki = 0.0000000;
   const float kd= 0.0;
   float error = error_calc();
   integral += error;
   derivative = error - last_error;
   float adjustSpeed = (error * kp) + (integral * ki) + (derivative * kd);
+  // MAIN BOT
   setMultipleMotors(base_speed + adjustSpeed, base_speed - adjustSpeed);
+  //BACKUP BOT setMultipleMotors(base_speed - adjustSpeed, base_speed + adjustSpeed);
   last_error = error;
+  counter++;
 }
